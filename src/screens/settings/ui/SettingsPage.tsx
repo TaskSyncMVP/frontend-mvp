1edbd59d4b50d059f0f29b1b4327173b91107d45
Sacerdot <nikita.byc@mail.ru>
2025-12-28 18:37:01 +0500

    #TSK-SNC-2: responsive: settings

--- src/screens/settings/ui/SettingsPage.tsx
+++ src/screens/settings/ui/SettingsPage.tsx
@@ -4,23 +4,29 @@ import {PageHeader} from "@/widgets";
 
 export function SettingsPage() {
     return (
-        <div>
-            <PageHeader title="Settings"/>
-            <div className="grid grid-rows-2 gap-6 mt-4 ">
-                <div>
-                    <h1 className="text-base font-semibold mb-3">Profile</h1>
-                    <div className="grid grid-cols-2 gap-x-4 gap-y-3">
-                        <Input placeholder="Email" type="email" />
-                        <Input placeholder="Password" type="password" />
-                        <Input placeholder="Name" />
-                    </div>
+        <div className="lg:flex lg:flex-col lg:justify-center lg:px-4">
+            <div>
+                <div className="text-center mb-8">
+                    <PageHeader title="Settings"/>
                 </div>
-                <div>
-                    <h1 className="text-base font-semibold mb-3">Pomodoro</h1>
-                    <div className="grid grid-cols-2 gap-x-4 gap-y-3">
-                        <Input placeholder="Work Interval"/>
-                        <Input placeholder="Interval Count"/>
-                        <Input placeholder="Break Interval"/>
+                <div className="flex justify-center">
+                    <div className="grid grid-rows-2 gap-6 w-full max-w-xl lg:max-w-2xl">
+                        <div>
+                            <h1 className="text-base font-semibold mb-3 lg:text-xl">Profile</h1>
+                            <div className="grid grid-cols-2 gap-x-4 gap-y-3">
+                                <Input placeholder="Email" type="email" />
+                                <Input placeholder="Password" type="password" />
+                                <Input placeholder="Name" />
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className="text-base font-semibold mb-3 lg:text-xl">Pomodoro</h1>
+                            <div className="grid grid-cols-2 gap-x-4 gap-y-3">
+                                <Input placeholder="Work Interval"/>
+                                <Input placeholder="Interval Count"/>
+                                <Input placeholder="Break Interval"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
