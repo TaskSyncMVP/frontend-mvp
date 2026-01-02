d5c99979e8af5ad40e13a2e5d48d279786820c42
Sacerdot <nikita.byc@mail.ru>
2026-01-02 15:26:21 +0500

    #TSK-SNC-2: Modal

--- src/shared/ui/dialog.tsx
+++ src/shared/ui/dialog.tsx
@@ -38,7 +38,7 @@ const DialogContent = React.forwardRef<
       ref={ref}
       className={cn(
         "fixed left-[50%] top-[50%] z-50 grid w-[calc(100vw-2rem)] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 " +
-          "border  bg-background pt-5 px-6 pb-3 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out " +
+          "bg-white pt-5 px-1 pb-3 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out " +
           "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 " +
           "data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 " +
           "data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 " +
@@ -52,6 +52,31 @@ const DialogContent = React.forwardRef<
   </DialogPortal>
 ))
 DialogContent.displayName = DialogPrimitive.Content.displayName
+
+const DialogContentPrimary = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <DialogPortal>
+    <DialogOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        "fixed left-[50%] top-[50%] z-50 grid w-[calc(100vw-2rem)] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 " +
+          "border bg-primary-100 pt-5 px-6 pb-3 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out " +
+          "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 " +
+          "data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 " +
+          "data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 " +
+          "data-[state=open]:slide-in-from-top-[48%] rounded-lg",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </DialogPrimitive.Content>
+  </DialogPortal>
+))
+DialogContentPrimary.displayName = "DialogContentPrimary"
 
 const DialogHeader = ({
   className,
@@ -88,7 +113,7 @@ const DialogTitle = React.forwardRef<
   <DialogPrimitive.Title
     ref={ref}
     className={cn(
-      "text-lg font-semibold leading-none tracking-tight",
+      "text-lg px-3 font-semibold leading-none tracking-tight",
       className
     )}
     {...props}
@@ -114,6 +139,7 @@ export {
   DialogOverlay,
   DialogTrigger,
   DialogContent,
+  DialogContentPrimary,
   DialogHeader,
   DialogFooter,
   DialogTitle,
